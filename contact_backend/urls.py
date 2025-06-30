e8247ddc74f305c28eb5ce68dc2db08a098f32d3
Niyad <niyadsystem@gmail.com>
2025-06-30 20:19:39 +0530

    upfront view of rest framework

--- contact_backend/urls.py
+++ contact_backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/contact/', include('contact_app.urls')),
+    path('', RedirectView.as_view(url='/api/contact/messages/', permanent=False), name='home'),
 ]
