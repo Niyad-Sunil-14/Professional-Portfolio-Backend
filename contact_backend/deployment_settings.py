e11193aae959a47bae3affb7d9df774ae2bd1881
Niyad <niyadsystem@gmail.com>
2025-06-30 20:59:43 +0530

    Email config done on production side

--- contact_backend/deployment_settings.py
+++ contact_backend/deployment_settings.py
@@ -45,7 +45,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER', default='Niyad Sunil <niyadfreelancer@gmail.com>')
-ADMIN_EMAIL = config('ADMIN_EMAIL', default='niyadfreelancer@gmail.com')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER', default='Niyad Sunil <niyadfreelancer@gmail.com>')
+ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL', default='niyadfreelancer@gmail.com')
