0661d39263a3cf6a1cd344c4aadf37ef32fd00c9
Niyad <niyadsystem@gmail.com>
2025-07-01 14:33:31 +0530

    True

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 
-if [[$CREATE_SUPERUSER]];
+if [[$CREATE_SUPERUSER == "true"]];
 then
     python manage.py createsuperuser --no-input
 fi
