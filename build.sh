163f7382949a8d69ad7b82a96970e808308a30da
Niyad <niyadsystem@gmail.com>
2025-07-01 15:29:44 +0530

    deleted

--- build.sh
+++ build.sh
@@ -7,8 +7,6 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 
-python create_superuser.py
-
 if [[$CREATE_SUPERUSER]];
 then
     python manage.py createsuperuser --no-input
