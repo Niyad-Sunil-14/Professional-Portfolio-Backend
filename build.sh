dc94db946d819f750a2db954e2b72963e648b33c
Niyad <niyadsystem@gmail.com>
2025-07-03 08:31:36 +0530

    Testing9

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 
-python manage.py createsuperuserauto
+python manage.py createsuperuser
 
 # if [[$CREATE_SUPERUSER]];
 # then
